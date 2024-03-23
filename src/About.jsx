61d4ff5a435708b8a979686bf093034238acded6
maheshbasnet089 <basnetmanish089@gmail.com>
2024-03-23 18:21:46 +0545

    learn about hooks,usestate

--- src/About.jsx
+++ src/About.jsx
@@ -1,10 +1,30 @@
+
+import { useState } from "react"
 import Button from "./Button"
 
 
 const About = () => {
+  // var count = 0 
+ const [number,setNumber] =  useState(0)
+//  const firstItem = state[0]
+//  const secondItem = state[1]
+//  console.log(firstItem,secondItem)
+  const inCount = () =>{    
+    setNumber(number + 1)
+   }
+
+   const decCount = () => {
+    setNumber(number-1)
+  }
+
   return (
+    // <div>
+    //   <h1>Hello world</h1>
+    // </div>
     <div>
-      <Button />
+      <h1>{number}</h1>
+      <button onClick={inCount} >+</button>
+      <button onClick={decCount} >-</button>
     </div>
   )
 }
